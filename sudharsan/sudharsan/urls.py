e6386cb04d7093e3e08cfa1a3419e400021d1f54
sudharsanakumar18 <sudharsan2004sr@gmail.com>
2023-07-26 14:13:50 +0000

    Sucess

--- sudharsan/sudharsan/urls.py
+++ sudharsan/sudharsan/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from myapp import views
 urlpatterns = [
     path('admin/' , admin.site.urls),
-    path('areaofrectangle/',views.rectarea,name="areaofrectangle"),
-    path('',views.rectarea,name="areaofrectangleroot")
+    path('areaoftriangle/',views.triarea,name="areaoftriangle"),
+    path('',views.triarea,name="areaoftriangleroot")
 ]
 
 
